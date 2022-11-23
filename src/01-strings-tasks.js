5a231ef19a08f37327a37534b090410187912dba
oleg <zerex21@icloud.com>
2022-11-23 15:48:20 +0300

    feat: add 1 task

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,10 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  /* throw new Error('Not implemented'); */
+  let res = value1 + "" + value2
+  return (res)
 }
 
 
